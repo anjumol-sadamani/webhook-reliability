7b7fecd338aef39b3c6652ecf6d879e44ec3b731
“anjumol” <anjumolms@gmail.com>
2026-09-12 11:05:58 +0200

    spring boot project init for tables - flyway script

---
+++ src/main/java/com/webhook_reliability/WebhookReliabilityApplication.java
@@ -0,0 +1,13 @@
+package com.webhook_reliability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebhookReliabilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebhookReliabilityApplication.class, args);
+	}
+
+}
