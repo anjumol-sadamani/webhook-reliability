ae85bda35401f2d8fe46cab1b7fed18aee05d83a
“anjumol” <anjumolms@gmail.com>
2026-09-15 16:15:43 +0200

    outbox publisher

--- src/main/java/com/webhook_reliability/WebhookReliabilityApplication.java
+++ src/main/java/com/webhook_reliability/WebhookReliabilityApplication.java
@@ -2,8 +2,10 @@ package com.webhook_reliability;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class WebhookReliabilityApplication {
 
 	public static void main(String[] args) {
